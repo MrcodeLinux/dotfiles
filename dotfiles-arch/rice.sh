e95ee108d5b2bc1726ef1f27d62fc89ebdb770bb
MrcodeLinux <117524136+MrcodeLinux@users.noreply.github.com>
2022-11-20 10:07:35 +0700

    Update rice.sh

--- dotfiles-arch/rice.sh
+++ dotfiles-arch/rice.sh
@@ -1,8 +1,14 @@
+
 makepkg -si
 sudo pacman -R $(pacman -Qdtq)   
-yay -S file-roller unzip unrar google-chrome sublime-text-4 vlc neofetch htop btop  picom ibus-bamboo lxappearance pulseaudio pavucontrol python-i3ipc
+yay -S file-roller unzip unrar google-chrome sublime-text-4 vlc neofetch htop btop  picom ibus-bamboo lxappearance pulseaudio pavucontrol python-i3ipc libreoffice-fresh
 chsh -s $(which fish)
 sudo chsh -s $(which fish)
+cd ~/
+git clone https://github.com/vinceliuice/Tela-circle-icon-theme.git
+cd Tela-circle-icon-theme
+chmod +x install.sh
+./install.sh yellow
 cd ~/
 git clone --depth=1 https://github.com/adi1090x/rofi.git
 cd ~/rofi
